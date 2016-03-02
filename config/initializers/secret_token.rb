6aa2477a0c06cf71bac78debcfd40a51770b7112
Che-cop <jguymon2002@yahoo.com>
2016-03-02 23:04:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tikster::Application.config.secret_key_base = '99581f811e276a24609e96a77b53f6748e16c821a4faf12d5e1d1c453caa65767e34529f17af06492ef164484ea2437be3b78c97f8586717ad86a8fa82268ef3'
