0e71966348ebfd8418f1a9cc338ab7b595dfe634
Che-cop <jguymon2002@yahoo.com>
2016-03-06 20:36:23 +0000

    added devise gem

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ gem 'kaminari'
 gem 'twitter-bootstrap-rails', :group => :assets
 
 gem 'simple_form'
+
+gem 'devise', '>=3.2.4'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Tikster::Application.routes.draw do
 
+
+
   root 'places#index'
 
   resources :places
+
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
