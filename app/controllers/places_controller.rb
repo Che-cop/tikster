abdb1a6f0f042fad630622e7b596143e290b4cd1
Che-cop <jguymon2002@yahoo.com>
2016-03-07 18:45:13 +0000

    connected users to places. added user email to new place post

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
-
+before_action :authenticate_user!, only: [:new, :create]
   def index
-    @places = Place.order(:id).page(params[:page])
+    @places = Place.order('updated_at DESC').page(params[:page]).per(7)
   end
 
   def new
@@ -9,7 +9,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
