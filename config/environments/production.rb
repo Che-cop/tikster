4f2f33a9c9f1741acc030f31ff8048dad84d7458
Che-cop <jguymon2002@yahoo.com>
2016-03-15 05:53:17 +0000

    updated prodution.rb to mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,19 @@
 Tikster::Application.configure do
+
+  Here is a sample configuration for ActionMailer.
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'tikster.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
+
   config.action_mailer.default_url_options = { host: 'https://tikster-jake-guymon.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
