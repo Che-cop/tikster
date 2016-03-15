7ff5cac34134072e34029aef45e7e0178c6f2d5f
Che-cop <jguymon2002@yahoo.com>
2016-03-15 04:22:33 +0000

    updated email with live email. updated comment added email message to include link

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@tiksterapp.com"
 
-def comment_added
-  mail(to: "jguymon2002@yahoo.com", subject: "A comment has been added to your place")
+def comment_added(comment)
+  @place = comment.place
+  @place_owner = comment.user
+  mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
 end
 
 
