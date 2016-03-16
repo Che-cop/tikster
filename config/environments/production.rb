74fb3ff30da9334d9372e85811b0333d55f573de
Che-cop <jguymon2002@yahoo.com>
2016-03-16 17:13:06 +0000

    updated rake file after error. updated app url for actionmailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Tikster::Application.configure do
 
 
 
-  config.action_mailer.default_url_options = { host: 'https://tikster-jake-guymon.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://tikster.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
