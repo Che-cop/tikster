0c1032d3a96f7bef23e328adec0fd698524e5bfd
Che-cop <jguymon2002@yahoo.com>
2016-03-17 17:57:26 +0000

    updated descrption validation to 600 char max

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,5 +8,5 @@ class Place < ActiveRecord::Base
 
   validates :name, :presence => {:message => " -Please enter the name of the bar-"}, :length => {:minimum => 2, :maximum => 100, :message => "-That name is to short or too long-"}
   validates :address, :presence => {:message => "-Please enter the address-"}, :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[ ])(?=.*[0-9]).{15,}\z/, :message => "-Please enter a valid address-"}, :uniqueness => {:message => "-That adrress has already been added-"}
-  validates :description, :presence => {:message => "-Please enter a little about this place-"}, :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[ ]).{10,}\z/, :message => "-Can you tell us a little more, please?-"}
+  validates :description, :presence => {:message => "-Please enter a little about this place-"}, :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[ ]).{10,}\z/, :message => "-Can you tell us a little more, please?-"}, :length => {:maximum => 600, :message => "-Sorry, 600 characters max-"}
 end
